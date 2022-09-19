384286f556db0d520bee1c0e26138f528f2e954d
sawanishiichiju <sawanishiichiju@localhost>
2022-09-19 14:37:55 +0900

    10章終了

--- JavaWcp2/src/practice/Car.java
+++ JavaWcp2/src/practice/Car.java
@@ -5,17 +5,19 @@ public class Car {
     private double fuelCost;
     // 残量（L）
     private double fuelAmount;
-
+    // コンストラクタを作成
     public Car(double fuelCost, double fuelAmount) {
         this.fuelCost = fuelCost;
         this.fuelAmount = fuelAmount;
     }
-
+    // moveメソッド
     public void move(int km) {
+    	// ・"xx km 走ります"を出力
         System.out.println(km + " km 走ります");
+     // ・残量を計算
         this.fuelAmount -= (km / fuelCost);
     }
-
+    // fuelAmountを取得するメソッドを作成
     public double getFuelAmount() {
         return this.fuelAmount;
     }
